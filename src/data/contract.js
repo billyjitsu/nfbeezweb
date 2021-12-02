0c4e2555fb238d70fbb1609272a243d80730ebed
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2021-12-01 19:58:50 -0800

    new contract test
    test

--- src/data/contract.js
+++ src/data/contract.js
@@ -1,4 +1,4 @@
 //export const contractAddress = "0xA795FbaE799BA8DA01f24B761BdC123E32Bebf2A";
 //export const contractAddress = "0x0255a82F9C09289843b0158C353F05BB86eA0099";  //xdai // striker
 //export const contractAddress = "0x085a44309A864094c6F9b13a9ba014d4800fF9e8";  //sokol
-export const contractAddress = "0x8b78DA1cDe27C12ABd8d103802cf51CE46826891"; // xdai
+export const contractAddress = "0xA795FbaE799BA8DA01f24B761BdC123E32Bebf2A"; // xdai
