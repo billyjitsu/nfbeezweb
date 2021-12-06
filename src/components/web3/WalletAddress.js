18d605145b1cbc3b43368f81e12e13f700cb1ddf
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2021-12-06 11:10:38 -0800

    fix no metamask error
    update wallet address logic to reflect no metamask error

--- src/components/web3/WalletAddress.js
+++ src/components/web3/WalletAddress.js
@@ -40,6 +40,7 @@ const WalletAddress = () => {
         );
     } else {
       setError(true);
+      setAccount("null");
     }
   };
 
@@ -92,13 +93,13 @@ const WalletAddress = () => {
   return (
     <>
       {error &&
-        !account(
+        account === "null"(
           <a
             href="https://metamask.io/download.html"
             rel={"noreferrer"}
             target="_blank"
           >
-            <Button>Download Metamask</Button>)
+            <Button>Download Metamask</Button>
           </a>
         )}
       {!account && (
