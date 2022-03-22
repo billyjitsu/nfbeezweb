027160c3c8ab32ceafb283ee32f7c552b665638c
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-03-22 10:30:08 -0700

    messing with wallet connect

--- src/components/web3/WalletAddress.js
+++ src/components/web3/WalletAddress.js
@@ -1,4 +1,6 @@
 import { ethers } from "ethers";
+import WalletConnectProvider from '@walletconnect/ethereum-provider';
+import { Web3Provider } from '@ethersproject/providers';
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import useStore from "../../store";
@@ -15,6 +17,8 @@ const Address = styled.p`
 const WalletAddress = () => {
   const [error, setError] = useState();
   const [account, setAccount] = useState();
+  const [ web3Library, setWeb3Library ] = useState(); //Provider
+  const [ walletConnectProvider, setWalletConnectProvider ] = useState();
   const [shortenedAddress, setShortenedAddress] = useState();
   const [wrongNetwork, setWrongNetwork] = useState(false);
   const addAddress = useStore((state) => state.addWalletAddress);
@@ -43,6 +47,35 @@ const WalletAddress = () => {
       setAccount("null");
     }
   };
+
+  const connectWalletConnect = async () => {
+		try {
+			const RPC_URLS = {
+				4: 'https://rinkeby.infura.io/v3/55d040fb60064deaa7acc8e320d99bd4',
+        100: 'https://rpc.gnosischain.com'
+			};
+			const provider = new WalletConnectProvider({
+				rpc: {
+					1: RPC_URLS[4],
+					4: RPC_URLS[100]
+				},
+				qrcode: true,
+				pollingInterval: 15000
+			});
+			setWalletConnectProvider(provider);
+			const accounts = await provider.enable();
+			const account = accounts[0];
+
+			const library = new Web3Provider(provider, 'any');
+
+			console.log('library', library);
+			setWeb3Library(library);
+			setAccount(account);
+      console.log("account:",accounts)
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
 
   const addWalletListener = async () => {
     if (window.ethereum) {
@@ -103,7 +136,10 @@ const WalletAddress = () => {
           </a>
         )}
       {!account && (
+        <>
         <Button onClick={connectWalletHandler}>Connect Wallet</Button>
+        <Button onClick={connectWalletConnect}>WalletConnect</Button>
+        </>
       )}
       {wrongNetwork && <Button onClick={connectToXDai}>Connect to gnosis</Button>}
       {!wrongNetwork && shortenedAddress && (
